fd57ea82d4f37c7e69b33db4a5aa3b91052750c6
lhp712 <tracker712@163.com>
2017-08-23 18:20:40 +0800

    测试test

--- app/src/main/java/com/lhp/test/MainActivity.java
+++ app/src/main/java/com/lhp/test/MainActivity.java
@@ -1,8 +1,12 @@
 package com.lhp.test;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("test","test");
+        ActivityManager amc= (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcessList = amc.getRunningAppProcesses();
+
     }
 }
